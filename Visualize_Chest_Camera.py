83a4d31026f841c7ed0dc99cc382d463732d7f1f
chongwilliam <wmchong@stanford.edu>
2021-06-22 18:38:55 -0700

    finished chest stream code

--- Visualize_Chest_Camera.py
+++ Visualize_Chest_Camera.py
@@ -3,11 +3,11 @@ import redis
 import numpy as np
 
 if __name__ == '__main__':
-	r = redis.Redis(host='127.0.0.1', port=6379)
+	r = redis.Redis(host='192.168.1.91', port=6379)
 
 	while True:
 		data = r.get('chest::camera')
-		img = np.frombuffer(data, dtype=np.uint8).reshape(720,1280,3)  # need to specify the exact shape here 
+		img = np.frombuffer(data, dtype=np.uint8).reshape(720,1280,3)  # need to specify the exact shape here
 
 		cv2.imshow("Image", img)
 		key = cv2.waitKey(1)
